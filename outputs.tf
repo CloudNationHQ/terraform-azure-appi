bde62b3edae039aaf76c086985b541f8d06a53a5
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2025-09-04 10:05:04 +0200

    fix: add missing outputs (#30)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,34 @@ output "api_keys" {
   description = "api keys for applications insights"
   value       = azurerm_application_insights_api_key.api_key
 }
+
+output "web_tests" {
+  description = "web tests for application insights"
+  value       = azurerm_application_insights_web_test.wt
+}
+
+output "standard_web_tests" {
+  description = "standard web tests for application insights"
+  value       = azurerm_application_insights_standard_web_test.swt
+}
+
+output "analytics_items" {
+  description = "analytics items for application insights"
+  value       = azurerm_application_insights_analytics_item.analytics_item
+}
+
+output "smart_detection_rules" {
+  description = "smart detection rules for application insights"
+  value       = azurerm_application_insights_smart_detection_rule.sdr
+}
+
+output "workbooks" {
+  description = "workbooks for application insights"
+  value       = azurerm_application_insights_workbook.wb
+}
+
+output "workbook_templates" {
+  description = "workbook templates for application insights"
+  value       = azurerm_application_insights_workbook_template.tmpl
+}
+
